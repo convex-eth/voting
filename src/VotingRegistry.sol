ed84358ad38bb3f335b442e119bf129636ff173a
c2tp <c2tp@protonmail.com>
2026-04-28 14:04:47 +0900

    add more contract types

--- src/VotingRegistry.sol
+++ src/VotingRegistry.sol
@@ -7,7 +7,11 @@ contract VotingRegistry is Ownable2Step {
     uint8 public constant VOTE_DAO = 0;
     uint8 public constant VOTE_GAUGE = 1;
     uint8 public constant GAUGE_REGISTRY = 2;
-    uint8 public constant OTHER_TYPE = 3;
+    uint8 public constant DAO_EXECUTOR = 3;
+    uint8 public constant GAUGE_EXECUTOR = 4;
+    uint8 public constant DAO_PROPOSER = 5;
+    uint8 public constant GAUGE_PROPOSER = 6;
+    uint8 public constant OTHER_TYPE = 7;
 
     event VotingContractSet(string indexed platform, uint8 indexed voteType, address indexed votingContract);
 
